b86f81c8ded64c5be178be5fa52333233b21371a
PapriKey <69516652+PapriKey@users.noreply.github.com>
2020-10-25 18:02:33 +0800

    Create 正序输出整数.cpp

---
+++ unsorted/正序输出整数.cpp
@@ -0,0 +1,28 @@
+/* 正序输出整数 */
+#include<stdio.h>
+#include <math.h>
+
+int main(void)
+{
+	long long int r_inp=0, i=1,w=0,inp=0,res=0;
+
+	scanf_s("%lld", &r_inp);
+	inp = r_inp;
+	while (r_inp > 9)
+	{
+		w++;
+		r_inp /= 10;
+	} 
+	w = pow(10, w); printf("%lld\n", w);
+
+	do 
+	{
+		res = inp / w;
+		printf("%lld ", res);
+		inp %= w;
+		w /= 10;
+
+	} while (w > 0);
+
+	return 0;
+}
