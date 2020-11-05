18a8d730441e924f1e85361de0c6c10c4ba55e18
PapriKey <69516652+PapriKey@users.noreply.github.com>
2020-11-05 13:02:12 +0800

    Update 七位显示器.cpp

--- unsorted/七位显示器.cpp
+++ unsorted/七位显示器.cpp
@@ -13,12 +13,12 @@ int num[10][7] = { {1,1,1,1,1,1,0},
                        {1,1,1,1,1,1,1},
                        {1,1,1,1,0,1,1} };
 
-int nub[10] = { -1,-1,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 };
+int nub[10] = { -1,-1,-1,-1,-1,-1,-1,-1,-1,-1 };
 
 
-int inpt(int r_inp)
+long long int inpt(int r_inp)
 {
-    int inp,w=0;
+    long long int inp,w=0;
     inp = r_inp;
     while (r_inp > 9)
     {
@@ -34,47 +34,92 @@ int inpt(int r_inp)
 int main(void)
 {
     long long int w, r_inp;
-    int res, i = 0;
-
-    scanf("%lld", &r_inp);
-
-    w = inpt(r_inp);
-
-    if (w >= 1000000000)
+    int res,i = 0;
+    for (;;)
     {
-        printf("数字过长！");
-        return 0;
-    }
+        i = 0;
+        printf("输入任意1~10位数字，他们将以七位显示器效果输出：");
+        scanf("%lld", &r_inp);
 
-    do
-    {
-        res = r_inp / w;
-        nub[i] = res;
-        r_inp %= w;
-        w /= 10;
-        i++;
-    } while (w > 0);
-    i = 0;
-    while (nub[i] != EOF)
-    {
+        if (r_inp < 0)
+            break;
 
-            if (num[nub[i]][0]) printf(" -- \n");
-            else printf("   \n");
+        w = inpt(r_inp);
+
+        if (w >= 1000000000)
+        {
+            printf("数字过长！");
+            return 0;
+        }
+
+        do
+        {
+            res = r_inp / w;
+            nub[i] = res;
+            r_inp %= w;
+            w /= 10;
+            i++;
+        } while (w > 0);
+
+        i = 0;
+
+        while (nub[i] != -1)
+        {
+            if (num[nub[i]][0]) printf(" --     ");
+            else printf("        ");
+            i++;
+        }
+        printf("\n");
+
+        i = 0;
+
+        while (nub[i] != -1)
+        {
             if (num[nub[i]][5]) printf("|  ");
             else printf("   ");
-            if (num[nub[i]][1]) printf("|\n");
-            else printf("  \n");
-            if (num[nub[i]][6]) printf(" -- \n");
-            else printf("   \n");
+            if (num[nub[i]][1]) printf("|    ");
+            else printf("     ");
+            i++;
+        }
+        printf("\n");
+
+        i = 0;
+
+        while (nub[i] != -1)
+        {
+            if (num[nub[i]][6]) printf(" --     ");
+            else printf("        ");
+            i++;
+        }
+        printf("\n");
+
+        i = 0;
+
+        while (nub[i] != -1)
+        {
             if (num[nub[i]][4]) printf("|  ");
             else printf("   ");
-            if (num[nub[i]][2]) printf("|\n");
-            else printf("  \n");
-            if (num[nub[i]][3]) printf(" -- \n");
-            else printf("   \n");
+            if (num[nub[i]][2]) printf("|    ");
+            else printf("     ");
             i++;
-    }
+        }
+        printf("\n");
 
+        i = 0;
+
+        while (nub[i] != -1)
+        {
+            if (num[nub[i]][3]) printf(" --     ");
+            else printf("        ");
+            i++;
+        }
+        printf("\n");
+
+        for (int i = 0; i < 10; i++)
+        {
+            nub[i] = -1;
+        }
+    }
     return 0;
 }
 
